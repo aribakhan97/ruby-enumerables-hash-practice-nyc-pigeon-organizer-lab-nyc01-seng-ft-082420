c6556ad45a3ec24aa77d10ea3a964a21259e4a25
Ariba Khan <akhan41@binghamton.edu>
2020-08-13 22:51:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,9 +7,13 @@ data.each_with_object({}) do |(key,value), final_array|
   value.each do |inner_key, names|
     names.each do |name|
       binding.pry
+      if !final_array[name]
+        final_array[name] = {}
 end
+if !final_array[name][key]
+  !final_array[name][key] = []
   end
   final_array
   binding.pry
 end
-end 
+end
